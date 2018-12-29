654b3a9c268a646abb3953d280a76163b75b713a
christophermp <meldre@outlook.com>
2018-12-29 09:31:49 +0100

    Update

--- MIMO_Examlpes/guess-the-word.py
+++ MIMO_Examlpes/guess-the-word.py
@@ -1,11 +1,18 @@
-master = "tnetennba"
+master = input("Write a word: ")
 word = list(master)
 length = len(word)
 right = list("_" * length)
-guess = input("Guess the letter! ")
-for letter in word:
-    if letter == guess:
-        index = word.index(guess)
-        right[index] = guess
-        word[index] = "_"
-print(right)
+finished = False
+while finished == False:
+    guess = input("Guess the letter! ")
+    if guess not in master:
+        print("This letter is not in the word.")
+    for letter in word:
+        if letter == guess:
+            index = word.index(guess)
+            right[index] = guess
+            word[index] = "_"
+    print(right)
+    if list(master) == right:
+        print("You win!")
+        finished = True
