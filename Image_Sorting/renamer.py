1da9b4afcae3d7475cbdb42e9cc48152573fe328
christophermp <meldre@outlook.com>
2018-12-30 08:58:22 +0100

    Update

--- Image_Sorting/renamer.py
+++ Image_Sorting/renamer.py
@@ -1,10 +1,14 @@
 import os
 from datetime import datetime
 folder = r"C:\Users\chris\Pictures"
-file = os.path.join(folder, "1.png")
-m_time = os.path.getmtime(file)
-real_time = datetime.fromtimestamp(m_time)
-print(real_time)
-print(m_time)
-f_time = datetime.strftime(real_time, "%y%m%d_%H%M%S")
-print(f_time)
+location = input("Photo Location: ")
+files = os.listdir(folder)
+for filename in files:
+    if not filename.startswith('.'):
+        file = os.path.join(folder, filename)
+    m_time = os.path.getmtime(file)
+    real_time = datetime.fromtimestamp(m_time)
+    f_time = datetime.strftime(real_time, "%y%m%d_%H%M%S")
+    new_filename = f_time + "_" + location + ".png"
+    new_file = os.path.join(folder, new_filename)
+    os.rename(file, new_file)
