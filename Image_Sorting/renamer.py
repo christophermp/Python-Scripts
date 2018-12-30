b0f0c6065bfe419ffd4a2ba477bd15dcc7a8f0c2
christophermp <meldre@outlook.com>
2018-12-30 08:43:23 +0100

    Update

--- Image_Sorting/renamer.py
+++ Image_Sorting/renamer.py
@@ -1,5 +1,10 @@
 import os
-folder = r"C:\Users\chris\Pictures\1.PNG"
-file = os.path.join(folder, "img1.png")
+from datetime import datetime
+folder = r"C:\Users\chris\Pictures"
+file = os.path.join(folder, "1.png")
 m_time = os.path.getmtime(file)
+real_time = datetime.fromtimestamp(m_time)
+print(real_time)
 print(m_time)
+f_time = datetime.strftime(real_time, "%y%m%d_%H%M%S")
+print(f_time)
